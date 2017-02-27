d5cd093439f6d78852a6f997ac8dbb73cedd8e97
laili <578516477@qq.com>
2017-02-28 00:45:29 +0800

    登录与登出功能的实现

--- js/common/main.js
+++ js/common/main.js
@@ -7,20 +7,11 @@ requirejs.config({
     paths:{
         jquery:"lib/jquery/jquery.min",
         bootstrap:"lib/bootstrap/js/bootstrap.min",
+        jqueryCookie: 'lib/jquery-cookie/jquery.cookie',
         //自己写的路径配置
         userList:"js/user/list",
         userProfile:"js/user/profile",
-        teacherAdd:"js/teacher/add",
-        teacherList:"js/teacher/list",
-        homeLogin:"js/home/login",
-        homeRepass:"js/home/repass",
-        homeReSettings:"js/home/settings",
-        courseAdd:"js/course/add",
-        courseAddStep1:"js/course/add_step1",
-        courseAddStep2:"js/course/add_step2",
-        courseAddStep3:"js/course/add_step3",
-        courseCategory:"js/course/category",
-        courseCategoryAdd:"js/course/category_add",
+        login:"js/home/login",
         common:"js/common/common",
     },
     shim:{
@@ -32,47 +23,33 @@ requirejs.config({
 //所有的页面都需要这两个js，先加载他们
 require(["jquery","bootstrap","common"]);
 //这里获取页面的URl，然后对应的加载js
-(function(){
+(function(window){
    var pathname=window.location.pathname;
-    switch (pathname){
-        case '/html/user/list.js':
-            require(["userList"]);
-            break;
-        case  '/html/user/profile.js':
-            require(["userProfile"]);
-            break;
-        case '/html/teacher/add.js':
-            require(["teacherAdd"]);
-            break;
-        case '/html/teacher/list.js':
-            require(["teacherList"]);
-            break;
-        case '/html/home/login.js':
-            require(["homeLogin"]);
-            break;
-        case '/html/home/repass.js':
-            require(["homeRepass"]);
-            break;
-        case '/html/home/settings.js':
-            require(["homeReSettings"]);
-            break;
-        case '/html/course/add.js':
-            require(["courseAdd"]);
-            break;
-        case '/html/course/add_step1.js':
-            require(["courseAddStep1"]);
-            break;
-        case '/html/course/add_step2.js':
-            require(["courseAddStep2"]);
-            break;
-        case '/html/course/add_step3.js':
-            require(["courseAddStep3"]);
-            break;
-        case '/html/course/category.js':
-            require(["courseCategory"]);
-            break;
-        case '/html/course/category_add.js':
-            require(["courseCategoryAdd"]);
-            break;
-    }
+    require(['jquery', 'jqueryCookie'],
+        function($,underfined){
+            var sessID= $.cookie("PHPSESSID");
+            //登陆状态前端效验
+            if(pathname==="/html/home/login.html"&&sessID){
+                location.href="/";
+            }else if(pathname!=="/html/home/login.html"&&!sessID){
+                location.href="/html/home/login.html";
+            };
+            switch (pathname){
+                case '/html/user/list.html':
+                    require(["userList"]);
+                    break;
+                case  '/html/user/profile.html':
+                    require(["userProfile"]);
+                    break;
+                //case '/html/teacher/add.html':
+                //    require(["teacherAdd"]);
+                //    break;
+                //case '/html/teacher/list.html':
+                //    require(["teacherList"]);
+                //    break;
+                case '/html/home/login.html':
+                    require(["login"]);
+                    break;
+            }
+        });
 })(window);
--- js/home/login.js
+++ js/home/login.js
@@ -1,3 +1,20 @@
-define(['jquery'], function($) {
-    console.log('login模块加载')
+define(['jquery','jqueryCookie'], function($,undefined) {
+    console.log('login模块加载');
+    $("#form-login").on("submit",function(){
+        $.ajax({
+            url:"/v6/login",
+            type:"post",
+            data:$(this).serialize(),
+            success:function(data){
+                console.log(111);
+                // 如果登陆成功，使用cookie的方式保存用户信息，
+                // 注意：cookie值必须为字符串，我们得到的是js对象，需要使用JSON.stringify进行转换
+                if(data.code===200){
+                    $.cookie("userInfo",JSON.stringify(data.result),{paths:"/"});
+                    location.href="/";
+                }
+            },
+        });
+        return false;
+    });
 });
