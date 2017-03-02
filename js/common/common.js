ee497dff2a1971c19ac8bcf7230889d8fbae386a
laili <578516477@qq.com>
2017-03-02 17:13:00 +0800

    提交

--- js/common/common.js
+++ js/common/common.js
@@ -6,18 +6,4 @@ define(["jquery"],function($){
     $(".navs a").on("click",function(){
         $(this).next().slideToggle();
     });
-    //$.ajax({
-    //    url:"/v6/login",
-    //    type:"post",
-    //    data:{
-    //        tc_name:123456,
-    //        tc_pass:123456,
-    //    },
-    //    success:function(){
-    //        console.log("≥…¡À");
-    //    },
-    //    error: function () {
-    //        console.log("∞‹¡À");
-    //    }
-    //})
 });
